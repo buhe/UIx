1935af30e82ea9b391cd15c263284458e8f6416e
buhe <bugu1986@126.com>
2023-09-01 16:52:06 +0800

    "Add remote source control dependency for \"swiftui-loading-view\" version 1.1.7 and update package platforms to iOS 15 and macOS 12."

--- Package.swift
+++ Package.swift
@@ -5,17 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "UIx",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UIx",
             targets: ["UIx"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/DanielMandea/swiftui-loading-view", .upToNextMajor(from: "1.1.7")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UIx"),
+            name: "UIx",
+            dependencies: [
+                .product(name: "LoadingView", package: "swiftui-loading-view"),
+            ]
+        ),
         .testTarget(
             name: "UIxTests",
             dependencies: ["UIx"]),
--- Sources/UIx/UIx.swift
+++ Sources/UIx/UIx.swift
@@ -1,2 +1,34 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import UIKit
+import SwiftUI
+import LoadingView
+
+extension View {
+    public func circleIndicatorWithSize(when binding: Binding<Bool>, lineWidth: CGFloat = 30, size: CGFloat, pathColor: Color, lineColor: Color, text: String) -> some View {
+        show(when: binding) {
+            // 显示过程
+            VStack {
+                CircleActivityView(lineWidth: lineWidth, pathColor: pathColor, lineColor: lineColor)
+                    .frame(width: size, height: size)
+                Text(text)
+            }
+        }
+    }
+}
+
+extension UIScreen {
+    static var screenWidth: CGFloat {
+        return main.bounds.width
+    }
+}
+
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+}
